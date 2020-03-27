aa29d2ff4dce84ad01d10f91e61917db5429670c
Nitish Patel <nitishp74@gmail.com>
2020-03-28 00:44:30 +0530

    Added Cart Backend

---
+++ cart/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Cart
+
+
+def cart_home(request):
+    cart_obj = Cart.objects.new_or_get(request)
+    
+    return render(request,"cart/index.html",{})
